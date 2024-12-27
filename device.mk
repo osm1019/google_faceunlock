f151e1fbc87c3397f67d2d67f143d1ad18bbea34
Oscar <osm1019@gmail.com>
2024-12-26 19:58:16 -0800

    Google Face Unlock: guard for Pixel devices only

--- device.mk
+++ device.mk
@@ -3,6 +3,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+ifneq ($(filter sailfish marlin walleye taimen blueline crosshatch sargo bonito flame coral sunfish bramble redfin barbet oriole raven bluejay panther cheetah lynx tangorpro felix shiba husky akita tokay caiman komodo comet frankel blazer mustang, $(LINEAGE_BUILD)),)
+
 # Do not include ROM side face unlock
 TARGET_FACE_UNLOCK_SUPPORTED := false
 
@@ -11,16 +13,25 @@ PRODUCT_PACKAGES += \
     SettingsGoogleFutureFaceEnroll
 
 # Overlay
+ifneq ($(TARGET_GOOGLE_FACE_UNLOCK),true)
 DEVICE_PACKAGE_OVERLAYS += vendor/google/faceunlock/overlay
+endif
 
 # Runtime resource overlays
+ifneq ($(TARGET_GOOGLE_FACE_UNLOCK),true)
 PRODUCT_PACKAGES += \
     FaceEnrollOverlay \
     FaceEnrollSettingsOverlay
+     
+endif
 
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.biometrics.face.xml
 
+
 # Sepolicy
+ifneq ($(TARGET_GOOGLE_FACE_UNLOCK),true)
 BOARD_SEPOLICY_DIRS += vendor/google/faceunlock/sepolicy
+endif
+endif
